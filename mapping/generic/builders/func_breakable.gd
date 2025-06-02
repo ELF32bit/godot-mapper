b0c8c6a2d5911eeed701ac492a6ed80d9be57a4a
Сергей Будяшов <sergeybudyashov@gmail.com>
2025-06-02 12:11:28 +0300

    Improved examples

--- mapping/generic/builders/func_breakable.gd
+++ mapping/generic/builders/func_breakable.gd
@@ -1,6 +1,7 @@
 @warning_ignore("unused_parameter")
 static func build(map: MapperMap, entity: MapperEntity) -> Node:
 	var node := Node3D.new()
+	MapperUtilities.apply_entity_transform(entity, node)
 
 	for brush in entity.brushes:
 		var brush_node := MapperUtilities.create_brush(entity, brush, "RigidBody3D")
