7be88085f31435627116a42b1b6b5b2081ef2262
ELF32bit <101274435+ELF32bit@users.noreply.github.com>
2025-08-10 15:43:04 +0300

    Added func_group parenting example

---
+++ mapping/generic/builders/func_group.gd
@@ -0,0 +1,27 @@
+@warning_ignore("unused_parameter")
+static func build(map: MapperMap, entity: MapperEntity) -> Node:
+	if not map.is_group_entity(entity, "_tb_group"):
+		return null
+
+	map.bind_group_entities(entity, "_tb_group")
+	var group_center := Vector3.ZERO
+	var count: int = 0
+
+	for group_entity in map.group_entities.get(entity, []):
+		group_entity.parent = entity
+
+	for group_entity in map.group_entities.get(entity, []):
+		if group_entity.brushes.size():
+			group_center += group_entity.center
+			count += 1
+
+	for brush in entity.brushes:
+		group_center += brush.center
+		count += 1
+
+	if count:
+		group_center /= count
+	entity.node_properties["position"] = group_center
+	entity.bind_string_property(map.settings.group_entity_name_property, "name")
+
+	return Node3D.new()
