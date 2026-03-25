f8397622c18f6c2fe2a21d0ed7dd896ff65720d7
Sergey <sergeybudyashov@gmail.com>
2026-03-25 11:12:11 +0300

    Updated grouping example

--- mapping/generic/builders/func_breakable.gd
+++ mapping/generic/builders/func_breakable.gd
@@ -9,6 +9,7 @@ static func build(map: MapperMap, entity: MapperEntity) -> Node:
 		var brush_node := MapperUtilities.create_brush(entity, brush, "RigidBody3D")
 		if not brush_node:
 			continue
+
 		# parenting one global node to another with right local coordinates
 		MapperUtilities.add_global_child(brush_node, node, map.settings)
 
